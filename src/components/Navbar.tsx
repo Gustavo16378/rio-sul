87f5a9ce7c34ce1e8dee73a389e39a731a12cce3
Gustavo16378 <gustavo16378@gmail.com>
2026-05-13 01:03:48 -0300

    feat: aumenta logo no desktop (h-14) e usa logo.jpeg como favicon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -137,7 +137,7 @@ export default function Navbar() {
           <img
             src="/photos/logo.jpeg"
             alt="Rio Sul Benefícios"
-            className="h-10 w-auto object-contain"
+            className="h-14 w-auto object-contain"
           />
         </Link>
 
